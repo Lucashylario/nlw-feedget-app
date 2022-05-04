40bcfb7b0e4f0bc6f969d248b268c7975c31f21f
Lucashylario <lucashyfrotend@hotmail.com>
2022-05-04 09:58:44 -0300

    add biblioteca de acessibilidade Headless UI

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,8 +1,12 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
+// import { useState } from 'react';
+
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+    /* Sem a Biblioteca de acessibilidade Headless UI */
+    /*const [isWidgetOpen, setIsWidgetOpen] = useState(false)
 
     function toggleWidgetVisibility() {
         setIsWidgetOpen(!isWidgetOpen)
@@ -17,5 +21,17 @@ export function Widget() {
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'><span className='pl-2' />Feedback</span>
             </button>
         </div>
+    )*/
+
+    /* Com a Biblioteca de acessibilidade Headless UI */
+    return (
+        <Popover className='absolute bottom-5 right-5'>
+            <Popover.Panel>Hello World</Popover.Panel>
+            
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6' />
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'><span className='pl-2' />Feedback</span>
+            </Popover.Button>
+        </Popover>
     )
 }
