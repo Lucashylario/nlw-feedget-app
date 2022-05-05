eb4993403eb2b8d7f69049664d5dd84ef2d492e3
Lucashylario <lucashyfrotend@hotmail.com>
2022-05-05 11:32:13 -0300

    finalizado funcionalidade do botão de enviar

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
-import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { ArrowLeft } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -14,6 +14,16 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested }:
 
     /* cod para armazenar a foto para enviar para o back end */
     const [screenshot, setScreenshot] = useState<string | null>(null)
+    // ------------------
+
+    /* Função do botão de enviar */ 
+    const [comment, setComment] = useState('')
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        console.log({screenshot, comment,})
+    }
+    // -----------
 
     return (
         <>
@@ -28,10 +38,11 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested }:
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea 
                 className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" 
                 placeholder="Conte com detalhes oque está acontecendo..." 
+                onChange={event => setComment(event.target.value)}
                 />
                 <footer className="flex gap-2 mt-2">
                     <ScreenshotButton
@@ -41,7 +52,8 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested }:
 
                     <button 
                     type="submit" 
-                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors font-semibold"
+                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors font-semibold disabled:opacity-50 disabled:hover:bg-brand-500"
+                    disabled={comment.length === 0 }
                     >
                         Enviar feedback
                     </button>
