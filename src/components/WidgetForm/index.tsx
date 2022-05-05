127071483cf3048e4095d5227e46adfb181deb79
Lucashylario <lucashyfrotend@hotmail.com>
2022-05-05 09:01:47 -0300

    add component dos botões

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -31,7 +32,7 @@ const feedbackTypes = {
     },
 }
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
@@ -45,21 +46,7 @@ export function WidgetForm() {
                 </header>
 
                 {!feedbackType ? (
-                    <div className="flex py-8 gap-2 w-full">
-                        { Object.entries(feedbackTypes).map(([key, value]) => {
-                            return (
-                                <button 
-                                    key={key}
-                                    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                                    onClick={() => setFeedbackType(key as FeedbackType)}
-                                    type="button"
-                                >
-                                    <img src={value.image.source} alt={value.image.alt} />
-                                    <span>{value.title}</span>
-                                </button>
-                            )
-                        }) }
-                    </div>
+                    <FeedbackTypeStep />
                 ) : (
                     <p>Olá</p>
                 )}
